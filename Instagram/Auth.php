5c16aac686cb6f57bfd229bda08b53a268e1a467
Philipp Tkachev <zoonman@gmail.com>
2014-02-21 11:40:22 -0500

    Small edit

--- Instagram/Auth.php
+++ Instagram/Auth.php
@@ -14,7 +14,7 @@ namespace Instagram;
  * Handles authentication
  * 
  * {@link https://github.com/galen/PHP-Instagram-API#authentication}
- * {@link https://github.com/galen/PHP-Instagram-API/blob/master/Examples/_auth.php}
+ * {@link https://github.com/marketmesuite/PHP-Instagram-API/blob/master/Examples/_auth.php}
  */
 class Auth {
 
